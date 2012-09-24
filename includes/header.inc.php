d0e5c1ccb60888a8be31c8cc136a69517fc83286
Pierce Moore <Pierce@PierceMoore.com>
2012-09-24 10:40:24 -0500

    Installed all assets, built shell website structure

---
+++ includes/header.inc.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8" />
+
+	<!-- Set the viewport width to device width for mobile -->
+	<meta name="viewport" content="width=device-width" />
+
+	<title><?=$title;?> :: Pierce Moore</title>
+
+	<!-- Included CSS Files (Uncompressed) -->
+	<!--
+	<link rel="stylesheet" href="stylesheets/foundation.css">
+	-->
+
+	<!-- Included CSS Files (Compressed) -->
+	<link rel="stylesheet" href="assets/css/foundation.min.css">
+	<link rel="stylesheet" href="assets/css/app.css">
+
+	<script src="assets/js/modernizr.foundation.js"></script>
+	<!-- Included JS Files (Compressed) -->
+	<script src="assets/js/jquery.js"></script>
+	<script src="assets/js/foundation.min.js"></script>
+
+	<!-- Initialize JS Plugins -->
+	<script src="assets/js/app.js"></script>
+
+	<link rel="stylesheet" href="assets/css/style.css" type="text/css" />
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,700|Oswald:300|Open+Sans+Condensed:300|Josefin+Slab:300,700' rel='stylesheet' type='text/css'>
+
+	<!-- IE Fix for HTML5 Tags -->
+	<!--[if lt IE 9]>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+
+
+<div class="row">
+	<div class="five columns">
+		<a class="logo" href="#">Pierce Moore</a>
+		<h5 style="margin-top:0;">great code, great food, great music, great company.</h5>
+	</div>
+
+	<div class="seven columns">
+		<br /><br />
+		<a href="#" class="btn">
+			Code
+		</a>
+		<a href="#" class="btn">
+			Music
+		</a>
+		<a href="#" class="btn">
+			Portfolio
+		</a>
+		<a href="#" class="btn">
+			Showcase
+		</a>
+	</div>
+</div>
---
+++ index.php
@@ -0,0 +1,30 @@
+<?php 
+
+$title = "Home";
+include( 'includes/header.inc.php' ); ?>
+
+
+<div class="row">
+
+	<div class="twelve columns">
+
+		<h1 class="huge_text">
+			Hey. What's Up?
+		</h1>
+
+	</div>
+
+</div>
+
+<div class="row">
+
+	<div class="eight columns">
+		Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+	</div>
+
+	<div class="four columns">
+		Four columns here
+	</div>
+</div>
+
+<?php include( 'includes/footer.inc.php' ); ?>
