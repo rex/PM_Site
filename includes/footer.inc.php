c3335a9c42d990e019a140a808fab6c3ff62bd3b
Pierce Moore <Pierce@PierceMoore.com>
2012-09-26 01:38:51 -0500

    Changed assets/img/ folder to assets/images/

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,6 +1,14 @@
+	<footer>
+		<div class="row">
+			<div class="ten columns">
 
+			</div>
 
-
+			<div class="two columns">
+				<p style="font-size: .9em;">&copy; 2012 Pierce Moore</p>
+			</div>
+		</div>
+	</footer>
 
 </body>
 
