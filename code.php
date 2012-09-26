d75645c3a2532302c45f3b6515845a701a45efde
Pierce Moore <Pierce@PierceMoore.com>
2012-09-26 14:02:10 -0500

    Building modal dialogs; removed slidey stuff

--- code.php
+++ code.php
@@ -13,12 +13,69 @@ $(window).load( function() {
 		captionAnimationSpeed: 	800
 	});
 
-	$(".expandable").click( function() {
-		$(".expandable div").slideUp('fast');
-		$(this).addClass('down').
+	/*
+	$(".toggle_exp").toggle( function() {
+		$(".expandable .exp_child").slideUp('fast');
+		$(this).addClass('down').text("Hide Code");
+		$(this).closest('.expandable').children('.exp_child').slideDown('fast');
+	}, function() {
+		$(".expandable .exp_child").slideUp('fast');
+		$(this).removeClass('down').text("See Code");
+		$(this).closest('.expandable').children('.exp_child').slideUp('fast');
 	});
+	*/
 });
 </script>
+
+<div id="snippet_1" class="snippet reveal-modal large">
+	<h1>Basic Reddit Reader in PHP</h1>
+	<script src="https://gist.github.com/3105963.js?file=redditReaderBasic.php"></script>
+
+	<a href="#" data-reveal-id="snippet_2" class="large button" style="float: right;">Next Snippet: Array to Object</a>
+	<a class="close-reveal-modal">x</a>
+</div>
+
+<div id="snippet_2" class="snippet reveal-modal large">
+	<h1>Recursively Convert a Multi-Dimentional PHP Array to an Object</h1>
+	<script src="https://gist.github.com/3107841.js?file=arrayToObject.php"></script>
+
+	<a href="#" data-reveal-id="snippet_1" class="large secondary button" style="float: left;">Last Snippet: PHP Reddit Reader</a>
+	<a href="#" data-reveal-id="snippet_1" class="large button" style="float: right;">Next Snippet: Array to Object</a>
+	<a class="close-reveal-modal">x</a>
+</div>
+
+<div id="snippet_3" class="snippet reveal-modal large">
+	<h1>Basic Reddit Reader in PHP</h1>
+	<script src="https://gist.github.com/3105963.js?file=redditReaderBasic.php"></script>
+
+	<a href="#" data-reveal-id="snippet_2" class="large button" style="float: right;">Next Snippet: Array to Object</a>
+	<a class="close-reveal-modal">x</a>
+</div>
+
+<div id="snippet_4" class="snippet reveal-modal large">
+	<h1>Convert a PHP Array to a StdClass Object</h1>
+	<script src="https://gist.github.com/3107841.js?file=arrayToObject.php"></script>
+
+	<a href="#" data-reveal-id="snippet_1" class="large secondary button" style="float: left;">Last Snippet: PHP Reddit Reader</a>
+	<a href="#" data-reveal-id="snippet_1" class="large button" style="float: right;">Next Snippet: Array to Object</a>
+	<a class="close-reveal-modal">x</a>
+</div>
+<div id="snippet_5" class="snippet reveal-modal large">
+	<h1>Basic Reddit Reader in PHP</h1>
+	<script src="https://gist.github.com/3105963.js?file=redditReaderBasic.php"></script>
+
+	<a href="#" data-reveal-id="snippet_2" class="large button" style="float: right;">Next Snippet: Array to Object</a>
+	<a class="close-reveal-modal">x</a>
+</div>
+
+<div id="snippet_6" class="snippet reveal-modal large">
+	<h1>Convert a PHP Array to a StdClass Object</h1>
+	<script src="https://gist.github.com/3107841.js?file=arrayToObject.php"></script>
+
+	<a href="#" data-reveal-id="snippet_1" class="large secondary button" style="float: left;">Last Snippet: PHP Reddit Reader</a>
+	<a href="#" data-reveal-id="snippet_1" class="large button" style="float: right;">Next Snippet: Array to Object</a>
+	<a class="close-reveal-modal">x</a>
+</div>
 
 <div class="row">
 
@@ -28,42 +85,14 @@ $(window).load( function() {
 			I write lots and lots of code.
 		</p>
 
-		<div class="expandable">
-			<div class="row">
-				<div class="ten columns">
-					<h1>Basic Reddit Reader in PHP</h1>
-				</div>
-				<div class="two columns">
-					<a href="#" class="btn small toggle_exp">	
-						Show
-					</a>	 
-				</div>
-			</div>
+		<a href="#" data-reveal-id="snippet_1" class="btn snippet">	
+			Basic Reddit Reader in PHP
+		</a>
 
-			<div>
-				<script src="https://gist.github.com/3105963.js?file=redditReaderBasic.php"></script>
-			</div>
-		</div>
-		<div class="expandable">
-			<div>
-				<script src="https://gist.github.com/3107841.js?file=arrayToObject.php"></script>
-			</div>
-		</div>
-		<div class="expandable">
-			<div>
-				<script src="https://gist.github.com/2292935.js?file=gistfile1.aw"></script>
-			</div>
-		</div>
-		<div class="expandable">
-			<div>
-				<script src="https://gist.github.com/2253239.js?file=gistfile1.aw"></script>
-			</div>
-		</div>
-		<div class="expandable">
-			<div>
-				<script src="https://gist.github.com/1969584.js?file=placefooter.jquery.js"></script>
-			</div>
-		</div>
+		<a href="#" data-reveal-id="snippet_2" class="btn snippet">	
+			Convert a PHP Array to an Object
+		</a>
+
 	</div>
 
 </div>
