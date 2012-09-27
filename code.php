9c670e00879d1317515b0e57c86f1dc0ffb3c56a
Pierce Moore <Pierce@PierceMoore.com>
2012-09-27 01:42:59 -0500

    Building music page

--- code.php
+++ code.php
@@ -32,7 +32,7 @@ $(window).load( function() {
 	<script src="https://gist.github.com/3105963.js?file=redditReaderBasic.php"></script>
 
 	<a href="#" data-reveal-id="snippet_2" class="large button" style="float: right;">Next Snippet: Array to Object</a>
-	<a class="close-reveal-modal">x</a>
+	<a class="close-reveal-modal normal alert button"> </a>
 </div>
 
 <div id="snippet_2" class="snippet reveal-modal large">
@@ -40,41 +40,34 @@ $(window).load( function() {
 	<script src="https://gist.github.com/3107841.js?file=arrayToObject.php"></script>
 
 	<a href="#" data-reveal-id="snippet_1" class="large secondary button" style="float: left;">Last Snippet: PHP Reddit Reader</a>
-	<a href="#" data-reveal-id="snippet_1" class="large button" style="float: right;">Next Snippet: Array to Object</a>
-	<a class="close-reveal-modal">x</a>
+	<a href="#" data-reveal-id="snippet_3" class="large button" style="float: right;">Next Snippet: Recursion in a Class</a>
+	<a class="close-reveal-modal normal alert button"> </a>
 </div>
 
 <div id="snippet_3" class="snippet reveal-modal large">
-	<h1>Basic Reddit Reader in PHP</h1>
-	<script src="https://gist.github.com/3105963.js?file=redditReaderBasic.php"></script>
+	<h1>Function Recursion within a Class / Object</h1>
+	<script src="https://gist.github.com/2292935.js?file=gistfile1.aw"></script>
 
-	<a href="#" data-reveal-id="snippet_2" class="large button" style="float: right;">Next Snippet: Array to Object</a>
-	<a class="close-reveal-modal">x</a>
+	<a href="#" data-reveal-id="snippet_2" class="large secondary button" style="float: left;">Last Snippet: Array to Object</a>
+	<a href="#" data-reveal-id="snippet_4" class="large button" style="float: right;">Next Snippet: Variable Function Arguments</a>
+	<a class="close-reveal-modal normal alert button"> </a>
 </div>
 
 <div id="snippet_4" class="snippet reveal-modal large">
-	<h1>Convert a PHP Array to a StdClass Object</h1>
-	<script src="https://gist.github.com/3107841.js?file=arrayToObject.php"></script>
+	<h1>Unlimited / Variable Number Function Arguments</h1>
+	<script src="https://gist.github.com/2253239.js?file=gistfile1.aw"></script>
 
-	<a href="#" data-reveal-id="snippet_1" class="large secondary button" style="float: left;">Last Snippet: PHP Reddit Reader</a>
-	<a href="#" data-reveal-id="snippet_1" class="large button" style="float: right;">Next Snippet: Array to Object</a>
-	<a class="close-reveal-modal">x</a>
+	<a href="#" data-reveal-id="snippet_3" class="large secondary button" style="float: left;">Last Snippet: Recursion in a Class</a>
+	<a href="#" data-reveal-id="snippet_5" class="large button" style="float: right;">Next Snippet: jQuery Footer Placement</a>
+	<a class="close-reveal-modal normal alert button"> </a>
 </div>
+
 <div id="snippet_5" class="snippet reveal-modal large">
-	<h1>Basic Reddit Reader in PHP</h1>
-	<script src="https://gist.github.com/3105963.js?file=redditReaderBasic.php"></script>
+	<h1>Keep the Footer at the Bottom of the Page with jQuery</h1>
+	<script src="https://gist.github.com/1969584.js?file=placefooter.jquery.js"></script>
 
-	<a href="#" data-reveal-id="snippet_2" class="large button" style="float: right;">Next Snippet: Array to Object</a>
-	<a class="close-reveal-modal">x</a>
-</div>
-
-<div id="snippet_6" class="snippet reveal-modal large">
-	<h1>Convert a PHP Array to a StdClass Object</h1>
-	<script src="https://gist.github.com/3107841.js?file=arrayToObject.php"></script>
-
-	<a href="#" data-reveal-id="snippet_1" class="large secondary button" style="float: left;">Last Snippet: PHP Reddit Reader</a>
-	<a href="#" data-reveal-id="snippet_1" class="large button" style="float: right;">Next Snippet: Array to Object</a>
-	<a class="close-reveal-modal">x</a>
+	<a href="#" data-reveal-id="snippet_4" class="large secondary button" style="float: left;">Last Snippet: Variable Function Arguments</a>
+	<a class="close-reveal-modal normal alert button"> </a>
 </div>
 
 <div class="row">
@@ -85,12 +78,29 @@ $(window).load( function() {
 			I write lots and lots of code.
 		</p>
 
+
+		<h2>&lt;?php ?&gt;</h2>
 		<a href="#" data-reveal-id="snippet_1" class="btn snippet">	
 			Basic Reddit Reader in PHP
 		</a>
 
 		<a href="#" data-reveal-id="snippet_2" class="btn snippet">	
 			Convert a PHP Array to an Object
+		</a>
+
+		<a href="#" data-reveal-id="snippet_3" class="btn snippet">	
+			Recursion in a Class
+		</a>
+
+		<a href="#" data-reveal-id="snippet_4" class="btn snippet">	
+			Variable Function Arguments
+		</a>
+
+		<div style="clear:both;"></div>
+
+		<h2>$(javascript);</h2>
+		<a href="#" data-reveal-id="snippet_5" class="btn snippet">	
+			jQuery Footer Placement
 		</a>
 
 	</div>
--- music.php
+++ music.php
@@ -3,25 +3,76 @@
 $title = "Music";
 include( 'includes/header.inc.php' ); ?>
 
-<script>
-$(window).load( function() {
-	$("#code_is").orbit({
-		bullets: 	true,
-		fluid: 		'16x6',
-		captions: 	true,
-		captionAnimation: 		'fade',
-		captionAnimationSpeed: 	800
-	});
-});
-</script>
+
+<div id="soundcloud" class="hidden reveal-modal large" style="background: url('<?=$url;?>/assets/images/icons/soundcloud_xxl.png' ) left bottom no-repeat #ccc; ">
+	<h2>Pierce Moore - Experiments in Ambience</h2>
+	<iframe width="100%" height="450" scrolling="no" frameborder="no" src="http://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Fplaylists%2F2326323&show_artwork=true"></iframe>
+	<a class="close-reveal-modal normal alert button"> </a>
+</div>
+
+<div id="mixcloud" class="hidden reveal-modal large" style="background: url('<?=$url;?>/assets/images/icons/mixcloud_xxl.gif' ) left bottom no-repeat #ccc; ">
+	<h2>Mixcloud <a href="#" class="small round secondary button">My MixCloud Profile</a></h2>
+	<hr />
+	<h4>Trance Heat 001</h4>
+	<div><object width="100%" height="125"><param name="movie" value="//www.mixcloud.com/media/swf/player/mixcloudLoader.swf?feed=http%3A%2F%2Fwww.mixcloud.com%2Fpiercemoore%2Fpierce-moore-trance-heat-001-03232012%2F&embed_uuid=51cd06f4-8416-4957-9c7a-f1539c595a11&stylecolor=&embed_type=widget_standard"></param><param name="allowFullScreen" value="true"></param><param name="wmode" value="opaque"></param><param name="allowscriptaccess" value="always"></param><embed src="//www.mixcloud.com/media/swf/player/mixcloudLoader.swf?feed=http%3A%2F%2Fwww.mixcloud.com%2Fpiercemoore%2Fpierce-moore-trance-heat-001-03232012%2F&embed_uuid=51cd06f4-8416-4957-9c7a-f1539c595a11&stylecolor=&embed_type=widget_standard" type="application/x-shockwave-flash" wmode="opaque" allowscriptaccess="always" allowfullscreen="true" width="100%" height="125"></embed></object><div style="clear:both; height:3px;"></div><p style="display:block; font-size:12px; font-family:Helvetica, Arial, sans-serif; margin:0; padding: 3px 4px; color:#02a0c7;"><a href="http://www.mixcloud.com/piercemoore/pierce-moore-trance-heat-001-03232012/?utm_source=widget&amp;utm_medium=web&amp;utm_campaign=base_links&amp;utm_term=resource_link" target="_blank" style="color:#02a0c7; font-weight:bold;">Pierce Moore - Trance Heat 001 - 03/23/2012</a><span> by </span><a href="http://www.mixcloud.com/piercemoore/?utm_source=widget&amp;utm_medium=web&amp;utm_campaign=base_links&amp;utm_term=profile_link" target="_blank" style="color:#02a0c7; font-weight:bold;">Pierce Moore</a><span> on </span><a href="http://www.mixcloud.com/?utm_source=widget&utm_medium=web&utm_campaign=base_links&utm_term=homepage_link" target="_blank" style="color:#02a0c7; font-weight:bold;"> Mixcloud</a></p><div style="clear:both; height:3px;"></div></div>
+	
+	<h4>Musings 02</h4>
+	<div><object width="100%" height="120"><param name="movie" value="//www.mixcloud.com/media/swf/player/mixcloudLoader.swf?feed=http%3A%2F%2Fwww.mixcloud.com%2Fpiercemoore%2Fpierce-moore-musings-02-02252012%2F&embed_uuid=c313646f-44ce-40e7-80cd-fda7dee1faea&stylecolor=&embed_type=widget_standard"></param><param name="allowFullScreen" value="true"></param><param name="wmode" value="opaque"></param><param name="allowscriptaccess" value="always"></param><embed src="//www.mixcloud.com/media/swf/player/mixcloudLoader.swf?feed=http%3A%2F%2Fwww.mixcloud.com%2Fpiercemoore%2Fpierce-moore-musings-02-02252012%2F&embed_uuid=c313646f-44ce-40e7-80cd-fda7dee1faea&stylecolor=&embed_type=widget_standard" type="application/x-shockwave-flash" wmode="opaque" allowscriptaccess="always" allowfullscreen="true" width="100%" height="120"></embed></object><div style="clear:both; height:3px;"></div><p style="display:block; font-size:12px; font-family:Helvetica, Arial, sans-serif; margin:0; padding: 3px 4px; color:#02a0c7;"><a href="http://www.mixcloud.com/piercemoore/pierce-moore-musings-02-02252012/?utm_source=widget&amp;utm_medium=web&amp;utm_campaign=base_links&amp;utm_term=resource_link" target="_blank" style="color:#02a0c7; font-weight:bold;">Pierce Moore - Musings 02 - 02/25/2012  </a><span> by </span><a href="http://www.mixcloud.com/piercemoore/?utm_source=widget&amp;utm_medium=web&amp;utm_campaign=base_links&amp;utm_term=profile_link" target="_blank" style="color:#02a0c7; font-weight:bold;">Pierce Moore</a><span> on </span><a href="http://www.mixcloud.com/?utm_source=widget&utm_medium=web&utm_campaign=base_links&utm_term=homepage_link" target="_blank" style="color:#02a0c7; font-weight:bold;"> Mixcloud</a></p><div style="clear:both; height:3px;"></div></div>
+
+	<h4>Musings 01</h4>
+	<div><object width="100%" height="120"><param name="movie" value="//www.mixcloud.com/media/swf/player/mixcloudLoader.swf?feed=http%3A%2F%2Fwww.mixcloud.com%2Fpiercemoore%2Fpierce-moore-musings-01-01112012%2F&embed_uuid=0468eb30-66cd-4450-b924-329cd2546092&stylecolor=&embed_type=widget_standard"></param><param name="allowFullScreen" value="true"></param><param name="wmode" value="opaque"></param><param name="allowscriptaccess" value="always"></param><embed src="//www.mixcloud.com/media/swf/player/mixcloudLoader.swf?feed=http%3A%2F%2Fwww.mixcloud.com%2Fpiercemoore%2Fpierce-moore-musings-01-01112012%2F&embed_uuid=0468eb30-66cd-4450-b924-329cd2546092&stylecolor=&embed_type=widget_standard" type="application/x-shockwave-flash" wmode="opaque" allowscriptaccess="always" allowfullscreen="true" width="100%" height="120"></embed></object><div style="clear:both; height:3px;"></div><p style="display:block; font-size:12px; font-family:Helvetica, Arial, sans-serif; margin:0; padding: 3px 4px; color:#02a0c7;"><a href="http://www.mixcloud.com/piercemoore/pierce-moore-musings-01-01112012/?utm_source=widget&amp;utm_medium=web&amp;utm_campaign=base_links&amp;utm_term=resource_link" target="_blank" style="color:#02a0c7; font-weight:bold;">Pierce Moore - Musings 01 - 01/11/2012</a><span> by </span><a href="http://www.mixcloud.com/piercemoore/?utm_source=widget&amp;utm_medium=web&amp;utm_campaign=base_links&amp;utm_term=profile_link" target="_blank" style="color:#02a0c7; font-weight:bold;">Pierce Moore</a><span> on </span><a href="http://www.mixcloud.com/?utm_source=widget&utm_medium=web&utm_campaign=base_links&utm_term=homepage_link" target="_blank" style="color:#02a0c7; font-weight:bold;"> Mixcloud</a></p><div style="clear:both; height:3px;"></div></div>
+
+	<a class="close-reveal-modal normal alert button"> </a>
+</div>
+
+<div id="spotify" class="hidden reveal-modal large" style="background: url('<?=$url;?>/assets/images/icons/spotify_xxl.png' ) left bottom no-repeat #ccc; ">
+	<h1>Spotify <a href="#" target="_blank" class="small round success button">My Spotify Profile</a></h1>
+
+	<hr />
+
+	<h1 class="styled">Pick a Playlist</h1>
+
+	<a href="#" data-reveal-id="" class="large success button">My Defining Playlist</a>
+	<a href="#" data-reveal-id="" class="large success button">My Album, 'Experiments in Ambience'</a>
+	<a href="#" data-reveal-id="" class="large success button">Chillgrim ( Epic Chillout Playlist )</a>
+	<a href="#" data-reveal-id="" class="large success button">Massive Chillout Playlist</a>
+
+	<a class="close-reveal-modal normal alert button"> </a>
+</div>
 
 <div class="row">
 
-	<div class="twelve columns">
+	<p class="huge_text" style="text-align:center;">
+		I sometimes do music stuff. Give it a listen. 
+	</p>
 
-		<p class="huge_text" style="text-align:center;">
-			Elvis is in the building. Well, he's on his way. 
-		</p>
+	<div class="eight columns">
+
+		<h1 class="styled">Listen</h1>
+
+		<a href="#" data-reveal-id="soundcloud" class="music_link" alt="Listen to my Music on " title="Listen to my Music on ">
+			<img src="<?=$url;?>/assets/images/icons/soundcloud_xxl.png" />
+		</a>
+
+		<a href="#" data-reveal-id="mixcloud" class="music_link" alt="Listen to my Music on " title="Listen to my Music on ">
+			<img src="<?=$url;?>/assets/images/icons/mixcloud_xxl.gif" />
+		</a>
+
+		<a href="#" data-reveal-id="spotify" class="music_link" alt="Listen to my Music on Spotify" title="Listen to my Music on Spotify">
+			<img src="<?=$url;?>/assets/images/icons/spotify_xxl.png" />
+		</a>
+
+	</div>
+
+	<div class="four columns">
+
+		<h1 class="styled">Purchase</h1>
+
+		<a href="http://itunes.apple.com/us/album/experiments-in-ambience/id551919580?uo=4" data-reveal-id="mixcloud" class="music_link" alt="Buy my Music on iTunes" title="Buy my Music on iTunes">
+			<img src="<?=$url;?>/assets/images/icons/itunes_xxl.png" />
+		</a>
+
+		
 
 	</div>
 
