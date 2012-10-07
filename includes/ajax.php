bea3f92e8dd91b18373136d32740807ffc5c93d7
Pierce Moore <Pierce@PierceMoore.com>
2012-10-06 21:25:08 -0500

    Enhancing the AJAX contact form

--- includes/ajax.php
+++ includes/ajax.php
@@ -6,10 +6,13 @@ class Resumailer {
 
 	function __construct( $email ) {
 
-		$this->to 		= "Pierce@piercemoore.com";
-		$this->from 	= "contactform@piercemoore.com";
-		$this->subject 	= "$email wants you to contact them.";
-		$this->message 	= "Here's the email address you need to contact: $email.";
+		$this->to 		= 	"Pierce@piercemoore.com";
+		$this->from 	= 	"contactform@piercemoore.com";
+		$this->subject 	= 	"$email wants you to contact them.";
+		$this->headers 	= 	"From: ContactForm@PierceMoore.com \r\n" .
+    						"Reply-To: noreply@piercemoore.com \r\n" .
+    						"X-Mailer: PHP/" . phpversion();
+		$this->message 	=	"Here's the email address you need to contact: $email.";
 
 	}
 
@@ -29,7 +32,7 @@ class Resumailer {
 
 $rm = new Resumailer( addslashes( $_REQUEST['email'] ) );
 if( $rm->send() ) {
-	print json_encode( array( "success" =>	true ) );
+	print json_encode( array( "success" =>	true , "msg" => "Everything worked just fine and I will contact you as quickly as possible!") );
 } else {
 	print json_encode( array( "success" => false , "msg" => $this->error ) );
 }
