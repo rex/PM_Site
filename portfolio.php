99007a84e12b51739b3622182183480ac2de7a9e
Pierce Moore <Pierce@PierceMoore.com>
2012-10-08 08:46:50 -0500

    Added four (4) projects to portfolio page

--- portfolio.php
+++ portfolio.php
@@ -16,7 +16,7 @@ include( 'includes/header.inc.php' ); ?>
 	<div class="five columns">
 		<a class="th">
 			<img src="http://placekitten.com/350/200" alt="Placeholder" title="Placeholder" >
-			<div class="port_caption"><p class="heading">&lsaquo;?php ?></p></div>
+			<div class="port_caption"><p class="heading">cms</p></div>
 		</a>
 	</div>
 	<div class="seven columns portfolio_listing">
@@ -32,14 +32,17 @@ include( 'includes/header.inc.php' ); ?>
 	<div class="five columns">
 		<a class="th">
 			<img src="http://placekitten.com/350/200" alt="Placeholder" title="Placeholder" >
-			<div class="port_caption"><p class="heading">&lsaquo;?php ?></p></div>
+			<div class="port_caption"><p class="heading">bantp</p></div>
 		</a>
 	</div>
 	<div class="seven columns portfolio_listing">
-		<p class="heading">CMS Project</p>
-		<p>Built a fully-featured CMS from scratch, complete with tiered user levels, messaging, notifications, and a javascript-based page building platform.</p>
+		<p class="heading">Bad Ass New Tab Page</p>
+		<p>I wrote a browser extension for Google Chrome that makes the new tab page a functional, beautiful place to make browsing more pleasant.</p>
 
-		<a href="#" data-reveal-id="portfolio_cms" class="large button port_open_info">Screenshots / Info</a>
+		<ul class="button-group rounded port_open_info">
+			<li><a href="#" class="large success button radius">Chrome Web Store</a></li>
+			<li><a href="#" data-reveal-id="portfolio_cms" class="large button radius">Screenshots / Info</a></li>
+		</ul>
 	</div>
 </div>
 
@@ -48,14 +51,18 @@ include( 'includes/header.inc.php' ); ?>
 	<div class="five columns">
 		<a class="th">
 			<img src="http://placekitten.com/350/200" alt="Placeholder" title="Placeholder" >
-			<div class="port_caption"><p class="heading">&lsaquo;?php ?></p></div>
+			<div class="port_caption"><p class="heading">rubyphp</p></div>
 		</a>
 	</div>
 	<div class="seven columns portfolio_listing">
-		<p class="heading">CMS Project</p>
-		<p>Built a fully-featured CMS from scratch, complete with tiered user levels, messaging, notifications, and a javascript-based page building platform.</p>
+		<p class="heading">RubyPHP</p>
+		<p>I wrote a really awesome PHP class that allows developers to harness the simplicity and syntax of Ruby in their codebase.</p>
 
-		<a href="#" data-reveal-id="portfolio_cms" class="large button port_open_info">Screenshots / Info</a>
+		
+		<ul class="button-group rounded port_open_info">
+			<li><a href="#" class="large success button radius">View on GitHub</a></li>
+			<li><a href="#" data-reveal-id="portfolio_cms" class="large button radius">Screenshots / Info</a></li>
+		</ul>
 	</div>
 </div>
 
@@ -64,12 +71,12 @@ include( 'includes/header.inc.php' ); ?>
 	<div class="five columns">
 		<a class="th">
 			<img src="http://placekitten.com/350/200" alt="Placeholder" title="Placeholder" >
-			<div class="port_caption"><p class="heading">&lsaquo;?php ?></p></div>
+			<div class="port_caption"><p class="heading">tickomatic</p></div>
 		</a>
 	</div>
 	<div class="seven columns portfolio_listing">
-		<p class="heading">CMS Project</p>
-		<p>Built a fully-featured CMS from scratch, complete with tiered user levels, messaging, notifications, and a javascript-based page building platform.</p>
+		<p class="heading">TickoMatic</p>
+		<p>I wrote a fun little application that provides users with a countdown timer and a serene image switching theme.</p>
 
 		<a href="#" data-reveal-id="portfolio_cms" class="large button port_open_info">Screenshots / Info</a>
 	</div>
